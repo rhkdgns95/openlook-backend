12f8eb3d43cba5311e3b8e758c3a88985fdb210f
kimkwanghoon <rhkdgns9489@gmail.com>
2021-05-22 21:15:00 +0900

    상수화

--- src/gateway/socket.io.gateway.ts
+++ src/gateway/socket.io.gateway.ts
@@ -11,11 +11,14 @@ import { Server, Socket } from "socket.io";
 import { RoomService } from "../service";
 
 enum Channel {
-  CREATE_ROOM = "CREATE_ROOM",
-  CREATE_USER = "CREATE_USER",
-  UPDATE_USER = "UPDATE_USER",
-  CONNECT_USER_TO_ROOM = "CONNECT_USER_TO_ROOM",
+  JOIN = "JOIN",
+  DISCONNECT = "DISCONNECT",
+  GET_CURRENT_ROOM = "GET_CURRENT_ROOM",
   JOIN_ROOM = "JOIN_ROOM",
+  NEW_USER = "NEW_USER",
+  RECEIVING_SIGNAL = "RECEIVING_SIGNAL",
+  SENDING_SIGNAL = "SENDING_SIGNAL",
+  RETURNING_SIGNAL = "RETURNING_SIGNAL"
 }
 
 const users = {};
@@ -39,7 +42,6 @@ export class SocketIoGateway
     this.logger.log("Initialized!");
   }
   handleConnection(client: Socket, ...args: any[]) {
-
     // this.logger.log("Client connected: ", client.id);
   }
 
@@ -47,20 +49,10 @@ export class SocketIoGateway
     console.log("handleDisconnect: ", socket.id);
     const roomDetail = this.roomService.deleteRoomDetailBySocketId(socket.id);
     this.wss.emit("DISCONNECT", roomDetail?.no);
-    // // console.log("disconnect client: ", client);
-    // const roomId = socketToRoom[socket.id];
-
-    // // console.log("BEFORE: ", users[roomId]);
-    // if (users[roomId]) {
-    //   users[roomId] = users[roomId].filter((v) => v.socketId !== socket.id);
-    // }
-    // console.log("AFTER: ", users[roomId]);
-
-    // this.wss.emit("ALREADY_USERS", users[roomId]);
   }
 
   /** 방참가 */
-  @SubscribeMessage("JOIN")
+  @SubscribeMessage(Channel.JOIN)
   join(socket: Socket, payload: { roomNo: number; positionNo: number }) {
     const room = this.roomService.updateRoom(payload.roomNo, {
       no: payload.positionNo,
@@ -71,38 +63,63 @@ export class SocketIoGateway
       room?.details?.filter((v) => v.socketId !== socket.id) || [];
     console.log("room: ", beforeRoom);
     console.log("beforeRoom: ", beforeRoom);
-    socket.emit("GET_CURRENT_ROOM", beforeRoom);
+    socket.emit(Channel.GET_CURRENT_ROOM, beforeRoom);
   }
 
-  @SubscribeMessage("JOIN_ROOM")
-  joinRoom(socket: Socket, payload: { roomId: number; positionId: number }) {
-    const { roomId, positionId } = payload;
-
-    console.log("join room - roomId: ", roomId);
-    console.log("join room - positionId: ", positionId);
-    if (users[roomId]) {
-      const length = users[roomId].length;
-      console.log("length: ", length);
-      if (length === 4) {
-        socket.emit("room full");
-        return;
-      }
-      users[roomId].push({ positionId, socketId: socket.id });
-      // users[roomId].push(socket.id);
-    } else {
-      users[roomId] = [{ positionId, socketId: socket.id }];
-    }
-    socketToRoom[socket.id] = roomId;
-
-    const usersInThisRooms = users[roomId].filter(
-      (v) => v.positionId !== positionId
-    );
-    // const usersInThisRoom = users[roomId].filter((id) => id !== socket.id);
-    console.log("users[roomId]: ", users[roomId]);
-    console.log("usersInThisRooms: ", usersInThisRooms);
-    socket.emit("ALREADY_USERS", usersInThisRooms);
-    // this.wss.emit(Channel.CREATE_USER, user);
+  @SubscribeMessage(Channel.SENDING_SIGNAL)
+  sendingSignal2(socket: Socket, payload) {
+    this.wss.to(payload.userToSignal).emit(Channel.NEW_USER, {
+      signal: payload.signal,
+      callerId: payload.callerId,
+      no: payload.no,
+    });
   }
+
+  @SubscribeMessage(Channel.RETURNING_SIGNAL)
+  returningSignal2(socket: Socket, payload) {
+    this.wss.to(payload.callerId).emit(Channel.RECEIVING_SIGNAL, {
+      signal: payload.signal,
+      id: socket.id,
+    });
+  }
+
+  // @SubscribeMessage(Channel.RECEIVING_SIGNAL)
+  // receivingSignal2(socket: Socket, payload) {
+  //   this.wss.to(payload.callerId).emit("receiving returned signal", {
+  //     signal: payload.signal,
+  //     id: socket.id,
+  //   });
+  // }
+
+  // @SubscribeMessage("JOIN_ROOM")
+  // joinRoom(socket: Socket, payload: { roomId: number; positionId: number }) {
+  //   const { roomId, positionId } = payload;
+
+  //   console.log("join room - roomId: ", roomId);
+  //   console.log("join room - positionId: ", positionId);
+  //   if (users[roomId]) {
+  //     const length = users[roomId].length;
+  //     console.log("length: ", length);
+  //     if (length === 4) {
+  //       socket.emit("room full");
+  //       return;
+  //     }
+  //     users[roomId].push({ positionId, socketId: socket.id });
+  //     // users[roomId].push(socket.id);
+  //   } else {
+  //     users[roomId] = [{ positionId, socketId: socket.id }];
+  //   }
+  //   socketToRoom[socket.id] = roomId;
+
+  //   // const usersInThisRooms = users[roomId].filter(
+  //   //   (v) => v.positionId !== positionId
+  //   // );
+  //   // const usersInThisRoom = users[roomId].filter((id) => id !== socket.id);
+  //   // console.log("users[roomId]: ", users[roomId]);
+  //   // console.log("usersInThisRooms: ", usersInThisRooms);
+  //   // socket.emit("ALREADY_USERS", usersInThisRooms);
+  //   // this.wss.emit(Channel.CREATE_USER, user);
+  // }
 
   // @SubscribeMessage("RECEIVING SIGNAL")
   // receivingSignal2(socket: Socket, payload) {
@@ -112,39 +129,21 @@ export class SocketIoGateway
   //   });
   // }
 
-  @SubscribeMessage("SENDING SIGNAL")
-  sendingSignal2(socket: Socket, payload) {
-    this.wss.to(payload.userToSignal).emit("USER JOIN", {
-      signal: payload.signal,
-      callerId: payload.callerId,
-      no: payload.no,
-    });
-  }
-
-  @SubscribeMessage("RETURNING SIGNAL")
-  returningSignal2(socket: Socket, payload) {
-    this.wss.to(payload.callerId).emit("RECEIVING SIGNAL", {
-      signal: payload.signal,
-      id: socket.id,
-    });
-  }
-
-
-  @SubscribeMessage("SENDING_SIGNAL")
-  sendingSignal(socket: Socket, payload) {
-    this.wss.to(payload.userToSignal).emit("USER_JOIN", {
-      signal: payload.signal,
-      callerId: payload.callerId,
-      positionId: payload.positionId,
-    });
-  }
-  @SubscribeMessage("returning signal")
-  returningSignal(socket: Socket, payload) {
-    this.wss.to(payload.callerId).emit("receiving returned signal", {
-      signal: payload.signal,
-      id: socket.id,
-    });
-  }
+  // @SubscribeMessage("SENDING_SIGNAL")
+  // sendingSignal(socket: Socket, payload) {
+  //   this.wss.to(payload.userToSignal).emit("USER_JOIN", {
+  //     signal: payload.signal,
+  //     callerId: payload.callerId,
+  //     positionId: payload.positionId,
+  //   });
+  // }
+  // @SubscribeMessage("returning signal")
+  // returningSignal(socket: Socket, payload) {
+  //   this.wss.to(payload.callerId).emit("receiving returned signal", {
+  //     signal: payload.signal,
+  //     id: socket.id,
+  //   });
+  // }
 
   // @SubscribeMessage("connect-room1")
   // createUser(client: Socket, data: any) {
